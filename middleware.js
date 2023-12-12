83d651be27d66aa91fa2846d938a8be0ccc1f29c
Rishi Raj <rishi@Rishis-MacBook-Air.local>
2023-12-13 00:37:34 +0530

    final product

--- middleware.js
+++ middleware.js
@@ -1,50 +1,53 @@
-const productSchema = require('./joiSchema');
-const Product = require('./models/product');
+const productSchema = require("./joiSchema");
+const Product = require("./models/product");
 
-module.exports.validateData = async (req , res , next)=>{
-    const {name, image, price, description} = req.body;
-    const { error, value } = await productSchema.validate({name, image, price, description});
+module.exports.validateData = async (req, res, next) => {
+  const { name, image, price, description } = req.body;
+  const { error, value } = await productSchema.validate({
+    name,
+    image,
+    price,
+    description,
+  });
 
-    if(error){
-        const msg = error.message;
-        return res.render('error', { err: msg });
-    }
-    next();
-}
+  if (error) {
+    const msg = error.message;
+    return res.render("error", { err: msg });
+  }
+  next();
+};
 
 module.exports.isLoggedIn = async (req, res, next) => {
-    if(req.xhr && !req.isAuthenticated()){
-        return res.status(401).json({
-            msg:'Plz login first'
-        })
-    }
+  if (req.xhr && !req.isAuthenticated()) {
+    return res.status(401).json({
+      msg: "Plz login first",
+    });
+  }
 
-    if (!req.isAuthenticated()) {
-        req.flash('error', 'Please LogIn first!!');
-        return res.redirect('/login');
+  if (!req.isAuthenticated()) {
+    req.flash("error", "Please LogIn first!!");
+    return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.isSeller = (req, res, next) => {
+  if (req.user && req.user.role === "buyer") {
+    return res.redirect("back");
+  }
+  next();
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id).populate("author");
+    if (product.author._id.toString() !== req.user._id.toString()) {
+      req.flash("error", "you are not authorized to do that");
+      return res.redirect("back");
     }
     next();
-}
-
-
-module.exports.isSeller = (req,res,next) =>{
-    
-    if(req.user && req.user.role === 'buyer'){
-        return res.redirect('back')
-    }
-    next();
-}
-
-module.exports.isAuthor = async (req,res,next)=>{
-try {
-        const {id} = req.params ;
-        const product = await Product.findById(id).populate('author');
-        if(product.author._id.toString() !== req.user._id.toString()){
-            req.flash('error', 'you are not authorized to do that');
-            return res.redirect('back');
-        }
-        next();
-} catch (error) {
-    console.log(error)
-}
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/apis/paymentApi.js
+++ routes/apis/paymentApi.js
@@ -1,53 +1,59 @@
-const router = require('express').Router();
-const Order = require('../../models/order');
-const Razorpay = require('razorpay');
-const { isLoggedIn } = require('../../middleware');
-const { validatePaymentVerification } = require('razorpay/dist/utils/razorpay-utils');
-const {RAZORPAY_KEY_ID, RAZORPAY_SECRET_KEY} = process.env;
+const router = require("express").Router();
+const Order = require("../../models/order");
+const Razorpay = require("razorpay");
+const { isLoggedIn } = require("../../middleware");
+const {
+  validatePaymentVerification,
+} = require("razorpay/dist/utils/razorpay-utils");
+const { RAZORPAY_KEY_ID, RAZORPAY_SECRET_KEY } = process.env;
 
-router.post('/order',isLoggedIn, async (req, res) => {
-    const instance = new Razorpay({ key_id: RAZORPAY_KEY_ID, key_secret:RAZORPAY_SECRET_KEY })
-    const { amount } = req.body
-    const options = {
-        amount: parseInt(amount) * 100 * 83.39,
-        currency: "INR"
-    }
-    const order = await instance.orders.create(options);
-    await Order.create({
-        _id: order.id,
-        user: req.user,
-        amount
-    })
+router.post("/order", isLoggedIn, async (req, res) => {
+  const instance = new Razorpay({
+    key_id: RAZORPAY_KEY_ID,
+    key_secret: RAZORPAY_SECRET_KEY,
+  });
+  const { amount } = req.body;
+  const options = {
+    amount: parseInt(amount) * 100,
+    currency: "INR",
+  };
+  const order = await instance.orders.create(options);
+  await Order.create({
+    _id: order.id,
+    user: req.user,
+    amount,
+  });
 
-    res.json({
-        sucess: true,
-        order
-    })
-
+  res.json({
+    sucess: true,
+    order,
+  });
 });
 
-router.post('/payment-verify/', async (req, res) => {
-    const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
-    const isValid = validatePaymentVerification({ "order_id": razorpay_order_id, "payment_id": razorpay_payment_id }, razorpay_signature, RAZORPAY_SECRET_KEY);
+router.post("/payment-verify/", async (req, res) => {
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+    req.body;
+  const isValid = validatePaymentVerification(
+    { order_id: razorpay_order_id, payment_id: razorpay_payment_id },
+    razorpay_signature,
+    RAZORPAY_SECRET_KEY
+  );
 
-    if (isValid) {
-        await Order.findByIdAndUpdate({ _id: razorpay_order_id }, { paymentStatus: true })
-        return res.status(200).json({
-            success: true,
-            msg: 'Payment successfull'
-        })
-    }
+  if (isValid) {
+    await Order.findByIdAndUpdate(
+      { _id: razorpay_order_id },
+      { paymentStatus: true }
+    );
+    return res.status(200).json({
+      success: true,
+      msg: "Payment successfull",
+    });
+  }
 
-    res.json({
-        succes: false,
-        msg: 'Payment unsuccessfull'
-    })
-})
-
-
-
-
-
-
+  res.json({
+    succes: false,
+    msg: "Payment unsuccessfull",
+  });
+});
 
 module.exports = router;
